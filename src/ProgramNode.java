153c6314c4db015d99de3dc6d6272ed1f861adc9
LaraibZafar <zafarhassan30@hotmail.com>
2020-12-25 18:08:37 +0500

    AST Almost done

---
+++ src/ProgramNode.java
@@ -0,0 +1,163 @@
+abstract class Node{}
+
+public class ProgramNode extends Node {
+    StatementNode statementNode;
+    public ProgramNode(StatementNode statementNode){
+        this.statementNode = statementNode;
+    }
+}
+
+class StatementNode extends Node{
+    StatementNode statementNode1;
+    StatementNode statementNode2;
+    AssignStatementNode assignStatementNode;
+    IfNode IfStatementNode;
+    WhileNode WhileStatementNode;
+
+    public StatementNode(StatementNode statementNode1){
+        this.statementNode1 = statementNode1;
+    }
+    public StatementNode(StatementNode statementNode1, StatementNode statementNode2){
+        this.statementNode1 = statementNode1;
+        this.statementNode2 = statementNode2;
+    }
+    public StatementNode(AssignStatementNode assignStatementNode){
+        this.assignStatementNode = assignStatementNode;
+    }
+    public StatementNode(IfNode IfStatementNode){
+        this.IfStatementNode = IfStatementNode;
+    }
+    public StatementNode(WhileNode WhileStatementNode){
+        this.WhileStatementNode = WhileStatementNode;
+    }
+}
+
+
+class AssignStatementNode extends Node{
+    IdentifierNode identifierNode;
+    EqualNode equalNode;
+    ExpressionNode expressionNode;
+    public AssignStatementNode(IdentifierNode identifierNode,EqualNode equalNode,ExpressionNode expressionNode){
+        this.identifierNode = identifierNode;
+        this.equalNode = equalNode;
+        this.expressionNode = expressionNode;
+    }
+}
+
+class EqualNode extends Node{
+    String name;
+    public EqualNode(){
+        this.name = "=";
+    }
+}
+
+class IfNode extends Node{
+    CompareStatementNode compareStatementNode;
+    StatementNode statementNode;
+    public IfNode(CompareStatementNode compareStatementNode, StatementNode statementNode){
+        this.compareStatementNode = compareStatementNode;
+        this.statementNode = statementNode;
+    }
+}
+
+class WhileNode extends Node{
+    CompareStatementNode compareStatementNode;
+    StatementNode statementNode;
+    public WhileNode(CompareStatementNode compareStatementNode, StatementNode statementNode){
+        this.compareStatementNode = compareStatementNode;
+        this.statementNode = statementNode;
+    }
+}
+
+class CompareStatementNode extends Node{ //Y
+    IdentifierNode identifierNode1;
+    CompareOperatorNode compareOperatorNode;
+    NumberNode numberNode;
+    IdentifierNode identifierNode2;
+    public CompareStatementNode(IdentifierNode identifierNode,CompareOperatorNode compareOperatorNode,NumberNode numberNode){
+        this.identifierNode1 = identifierNode;
+        this.compareOperatorNode = compareOperatorNode;
+        this.numberNode = numberNode;
+    }
+    public CompareStatementNode(IdentifierNode identifierNode1,CompareOperatorNode compareOperatorNode,IdentifierNode identifierNode2){
+        this.identifierNode1 = identifierNode1;
+        this.compareOperatorNode = compareOperatorNode;
+        this.identifierNode2 = identifierNode2;
+    }
+
+}
+
+
+class CompareOperatorNode extends Node{
+    String name;
+    public CompareOperatorNode(String name){
+        this.name=name;
+    }
+}
+
+class ExpressionNode extends Node{
+    ExpressionNode expressionNode;
+    EqualNode equalNode;
+    TermNode termNode;
+    public ExpressionNode(ExpressionNode expressionNode,EqualNode equalNode,TermNode termNode){
+        this.expressionNode = expressionNode;
+        this.equalNode = equalNode;
+        this.termNode = termNode;
+    }
+    public ExpressionNode(TermNode termNode){
+        this.termNode = termNode;
+    }
+}
+
+class Operation1Node extends Node{
+    String operator;
+    public Operation1Node(String operator){
+        this.operator = operator;
+    }
+}
+
+class Operation2Node extends Node{
+    String operator;
+    public Operation2Node(String operator){
+        this.operator = operator;
+    }
+}
+
+class TermNode extends Node{
+    TermNode termNode;
+    Operation2Node operation2Node;
+    FactorNode factorNode;
+    public TermNode(TermNode termNode,Operation2Node operation2Node,FactorNode factorNode){
+        this.termNode = termNode;
+        this.operation2Node = operation2Node;
+        this.factorNode = factorNode;
+    }
+    public TermNode(FactorNode factorNode){
+        this.factorNode = factorNode;
+    }
+}
+
+class FactorNode extends Node{
+    ExpressionNode expressionNode;
+    IdentifierNode identifierNode;
+    public FactorNode(ExpressionNode expressionNode){
+        this.expressionNode=expressionNode;
+    }
+    public FactorNode(IdentifierNode identifierNode){
+        this.identifierNode = identifierNode;
+    }
+}
+
+class NumberNode extends Node{
+    int number;
+    public NumberNode(int number){
+        this.number = number;
+    }
+}
+
+class IdentifierNode extends Node{
+    String identifierName;
+    public IdentifierNode(String identifierName){
+        this.identifierName = identifierName;
+    }
+}
--- src/smolParser.java
+++ src/smolParser.java
@@ -1,8 +1,9 @@
+import javax.swing.plaf.nimbus.State;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Stack;
-
+import java.util.ArrayList;
 import static java.lang.System.exit;
 
 import java.util.HashMap;
@@ -77,45 +78,61 @@ class parser{
     String[][] grammarRules= new String[32][2];
     String[] random;
     token[] tokenArray;
+    ArrayList<String> poppedTokens =new ArrayList<String>();
+    ArrayList<tokenNode> poppedTokens2 =new ArrayList<tokenNode>();
     HashMap<String, String> TYPETOSTRING;
     public int LrParser(HashMap<String, String> TYPETOSTRING){
         Stack<String> stack= new Stack<>();
+        Stack<tokenNode> stack2= new Stack<>();
         this.TYPETOSTRING = TYPETOSTRING;
         tokenArray= compiilerScanner.main(random);
         //printTokenArray(tokenArray);
         int tokenCount=0;
         String operation;
-        String token = getToken(tokenCount++);
-        if(token.equals("\0")){
+        //String token = getToken(tokenCount++);
+        tokenNode token2 = new tokenNode(getToken2(tokenCount++));
+//        if(token.equals("\0")){
+//            return -1;
+//        }
+        if(token2.scannerToken ==null){
             return -1;
         }
-        stack.push("1");
+//        stack.push("1");
+        stack2.push(new tokenNode(1));
         while(true){
-            //System.out.println(stack.toString());
-            int currentState= Integer.parseInt(stack.peek());
+//            int currentState= Integer.parseInt(stack.peek());
+                int currentState2 = stack2.peek().stateNumber;
             //currentState--;
-            int tokenColumn = columnOf(transitionTable[0],""+token);
-            if(transitionTable[currentState][tokenColumn]==null){
-                System.out.println("Error at : " + token);
-                exit(0);
-            }
-            if(transitionTable[currentState][tokenColumn].charAt(0)=='s'){          //shift
-                operation = transitionTable[currentState][tokenColumn];
-                stack.push(""+token);
+//            int tokenColumn = columnOf(transitionTable[0],""+token);
+            int tokenColumn2 = columnOf(transitionTable[0],""+token2.name);
+//            if(transitionTable[currentState][tokenColumn]==null){
+//                System.out.println("Error at : " + token);
+//                exit(0);
+//            }
+            if(transitionTable[currentState2][tokenColumn2].charAt(0)=='s'){          //shift
+                operation = transitionTable[currentState2][tokenColumn2];
+//                stack.push(""+token);
+                stack2.push(token2);
                 if(operation.length()>2){
-                    stack.push(""+operation.charAt(1)+""+operation.charAt(2));
+//                    stack.push(""+operation.charAt(1)+""+operation.charAt(2));
+                    stack2.push(new tokenNode(Integer.parseInt(""+operation.charAt(1)+""+operation.charAt(2))));
                 }
                 else {
-                    stack.push("" + operation.charAt(1));
+//                    stack.push("" + operation.charAt(1));
+                    stack2.push(new tokenNode(Integer.parseInt("" + operation.charAt(1))));
                 }
-                token = getToken(tokenCount++);
-                if(token.equals("\0")){
+//                token = getToken(tokenCount++);
+                token2 = new tokenNode(getToken2(tokenCount++));
+//                if(token.equals("\0")){
+//                    return -1;
+//                }
+                if(token2.scannerToken ==null){
                     return -1;
                 }
                 //System.out.println(stack.toString());
             }
-            else if(transitionTable[currentState][tokenColumn].charAt(0)=='r'){
-                operation = transitionTable[currentState][tokenColumn];
+            else if(transitionTable[currentState2][tokenColumn2].charAt(0)=='r'){
+                operation = transitionTable[currentState2][tokenColumn2];
                 int grammarRuleNumber;
                 if(operation.length()>2) {
                     grammarRuleNumber = Integer.parseInt(""+operation.charAt(1)+""+operation.charAt(2));
@@ -127,22 +144,31 @@ class parser{
                 String LHS = grammarRules[grammarRuleNumber][0];
                 String RHS = grammarRules[grammarRuleNumber][1];
                 for (int i = 0; i < 2* RHS.length() ; i++) {                    //Pop the Character & the state
-                    stack.pop();
+                    if(i%2==0) {      //Don't want the index numbers
+//                        stack.pop();
+                        stack2.pop();
+                    }
+                    else{
+//                        poppedTokens.add(stack.pop());
+                        poppedTokens2.add(stack2.pop());
+                    }
                 }
-                currentState = Integer.parseInt(stack.peek());
-                tokenColumn = columnOf(transitionTable[0],LHS);
-                stack.push(LHS);
+                printPoppedTokens();
+//                currentState = Integer.parseInt(stack.peek());
+                currentState2 = stack2.peek().stateNumber;
+                tokenColumn2 = columnOf(transitionTable[0],LHS);
+                stack2.push(new tokenNode(LHS));
                 //System.out.println(currentState);
                 //System.out.println(tokenColumn);
-                if(transitionTable[currentState][tokenColumn].length()>2) {
-                    stack.push(""+transitionTable[currentState][tokenColumn].charAt(1)+""+transitionTable[currentState][tokenColumn].charAt(2));     //Transition on State when char = reduction char
+                if(transitionTable[currentState2][tokenColumn2].length()>2) {
+                    stack2.push(new tokenNode(Integer.parseInt(""+transitionTable[currentState2][tokenColumn2].charAt(1)+""+transitionTable[currentState2][tokenColumn2].charAt(2))));     //Transition on State when char = reduction char
                 }
                 else{
                     //System.out.println(token);
-                    stack.push(""+transitionTable[currentState][tokenColumn].charAt(1));     //Transition on State when char = reduction char
+                    stack2.push(new tokenNode(Integer.parseInt(""+transitionTable[currentState2][tokenColumn2].charAt(1))));     //Transition on State when char = reduction char
                 }
             }
-            else if(transitionTable[currentState][tokenColumn].equals("acc")) {
+            else if(transitionTable[currentState2][tokenColumn2].equals("acc")) {
                 printTokenArray(tokenArray);
                 return 1;
             }
@@ -151,6 +177,44 @@ class parser{
                 return -1;
             }
         }
+    }
+
+    public Node initializeNode(ArrayList<tokenNode> poppedTokens, String LHS, Stack<tokenNode> stack2){
+        if(LHS.equals("P")){
+            ProgramNode PNode = new ProgramNode((StatementNode) poppedTokens.get(0).node);
+            stack2.push(new tokenNode(PNode,LHS));
+        }
+        else if(LHS.equals("S")){
+            StatementNode SNode=null;
+            if(poppedTokens.size()<2) {
+                if(poppedTokens.get(0).name.equals("S"))
+                    SNode = new StatementNode((StatementNode) poppedTokens.get(0).node);
+                else if (poppedTokens.get(0).name.equals("A"))
+                    SNode = new StatementNode((AssignStatementNode) poppedTokens.get(0).node);
+            }
+            else{
+                SNode = new StatementNode((StatementNode) poppedTokens.get(0).node,(StatementNode) poppedTokens.get(1).node);
+            }
+            stack2.push(new tokenNode(SNode,LHS));
+
+        }
+        else if(LHS.equals("A")){}
+        else if(LHS.equals("X")){}
+        else if(LHS.equals("L")){}
+        else if(LHS.equals("W")){}
+        else if(LHS.equals("Y")){}
+        else if(LHS.equals("C")){}
+        else if(LHS.equals("E")){}
+        else if(LHS.equals("O")){}
+        else if(LHS.equals("T")){}
+        else if(LHS.equals("U")){}
+        else if(LHS.equals("F")){}
+        else if(LHS.equals("I")){}
+        else if(LHS.equals("N")){}
+
+
+
+        return null;
     }
 
     public String[][] populateTransitionTable(){
@@ -278,6 +342,18 @@ class parser{
             return "\0";
         }
     }
+    public token getToken2(int tokenCount) {
+        if (tokenCount < tokenArray.length) {
+            //System.out.println(TYPETOSTRING.get(tokenArray[tokenCount].tokenType.ordinal() + ""));
+            String actionTableValue = TYPETOSTRING.get(tokenArray[tokenCount].tokenType.ordinal() + "");
+            tokenArray[tokenCount].actionTableValue = actionTableValue;
+            return tokenArray[tokenCount];
+
+        } else {
+            return null;
+        }
+    }
+
     public void printTokenArray(token[] tokenArray){
         System.out.println("\n\nSCANNER => TOKEN ARRAY:");
         for(token currentToken : tokenArray){
@@ -286,14 +362,39 @@ class parser{
         }
         System.out.println("");
     }
+    public void printPoppedTokens(){
+        System.out.println("\n Popped Tokens Array");
+        for(String currentToken : poppedTokens){
+            System.out.println(currentToken);
+        }
+        poppedTokens.clear();
+        System.out.println("");
+    }
 }
 
 class tokenNode{
-    token token;
+    token scannerToken;
+    int stateNumber;
+    Node node;
+    String type;
+    String name;
 
-    public tokenNode(token token){
-        this.token = token;
+    public tokenNode(token scannerToken){
+        this.scannerToken = scannerToken;
+        name = scannerToken.actionTableValue;
+        type = "token";
     }
+    public tokenNode(int stateNumber){
+        this.stateNumber = stateNumber;
+        type = "token";
+    }
+    public tokenNode(Node node, String name){
+        this.node = node;
+        type = "int";
+        this.name = name; //left side of production
+    }
+
+
 }
 
 
